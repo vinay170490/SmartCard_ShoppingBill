53343891c6b859d1f1a8333c415922833970f5c8
kowko <kowko@DESKTOP-5K33MMA>
2019-05-12 23:24:28 +0530

    few more code changes based on requirement

--- src/com/smartcard/ReadJson.java
+++ src/com/smartcard/ReadJson.java
@@ -18,7 +18,18 @@ public class ReadJson extends ProductList {
 		JSONArray productList = products.getProductList();
 		List<Bill> billItems = calculateBill.addProductsToBill(productList);
 	
-
-		System.out.println("Total Bill is "+ billItems.toString());
+		System.out.println("Selected Items are :");
+		double totalCost = 0;
+		double serviceTax = 0;
+ for(int i = 0; i<billItems.size();i++) {
+	 System.out.println(billItems.get(i).getProductName()+" "+
+			 billItems.get(i).getNoOfItems()+" "+billItems.get(i).getProductCost());
+	 		serviceTax = serviceTax+billItems.get(i).getProductTax();
+	 		totalCost = totalCost + billItems.get(i).getProductCost();
+	 
+ 	}
+ 	totalCost = totalCost+serviceTax;
+	System.out.println("Service Tax : " + serviceTax);
+	System.out.println("Total Bill : " + totalCost);
 	}
 }
